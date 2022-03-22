b4138381ee633235ab814ae202ed66cd41b55ff2
Lu Ye <alison94713@gmail.com>
2022-03-22 13:01:39 -0400

    hotels

--- src/Pages/SearchResultsPage/SearchResultsPage.jsx
+++ src/Pages/SearchResultsPage/SearchResultsPage.jsx
@@ -1,6 +1,7 @@
 import './SearchResultsPage.css'
 import React from 'react'
 import Footer from '../../Components/Footer/Footer'
+import Hotels from '../../Components/Hotels/Hotels'
 
 export default function SearchResultsPage(props) {
 
@@ -8,6 +9,7 @@ export default function SearchResultsPage(props) {
     return (
         <div className='SearchResultsPage'>
             <h1>SearchResultsPage</h1>
+            <Hotels />
             <Footer />
         </div>
     )
