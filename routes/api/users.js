113d95d2bd3158d587121bc36a9ada6ce0a73796
Josh <mpphuoc@gmail.com>
2022-03-22 10:58:19 -0400

    add login and signup

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const usersCtrl = require('../../controllers/api/users')
 
-
+router.get('/', usersCtrl.getOne)
 router.get('/all', usersCtrl.getAll)
 
 
