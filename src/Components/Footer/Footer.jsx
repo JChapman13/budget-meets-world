2db632b894e396509d935842211a9e6bcc15484f
JChapman13 <77743972+JChapman13@users.noreply.github.com>
2022-03-22 19:48:29 -0400

    adding restaurants api call

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,22 +1,18 @@
-import './Footer.css'
-import React from 'react'
-
-
+import "./Footer.css";
+import React from "react";
 
 export default function Footer(props) {
-
-
-    return (
-        <div className='footer'>
-            <div>
-                <p>MY TRIPS</p>
-            </div>
-            <div>
-                <p>SEARCH</p>
-            </div>
-            <div>
-                <p>PROFILE</p>
-            </div>
-        </div>
-    )
+  return (
+    <div className="footer">
+      <div>
+        <p>MY TRIPS</p>
+      </div>
+      <div>
+        <p>SEARCH</p>
+      </div>
+      <div>
+        <p>PROFILE</p>
+      </div>
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './Pages/App/App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "../src/pages/App/App";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,6 +9,5 @@ ReactDOM.render(
       <App />
     </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
