be6ab0e8f5670c6036a319a7735e64d8bc780463
Lu Ye <alison94713@gmail.com>
2022-03-22 13:11:30 -0400

    testing

--- src/Components/Hotels/Hotels.jsx
+++ src/Components/Hotels/Hotels.jsx
@@ -1,14 +1,15 @@
 import './Hotels.css'
 import React from 'react'
-
+import Hotel from '../Hotel/Hotel'
 
 
 export default function Hotels(props) {
-
+    
 
     return (
         <div className='Hotels'>
             <p>Hotels</p>
+            <Hotel />
         </div>
     )
 }
