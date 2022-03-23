42e302f07d806779286e58e75d1cc4e646ebc5c9
Lu Ye <alison94713@gmail.com>
2022-03-23 14:55:03 -0400

    added more photos

--- src/Pages/HotelDetailPage/HotelDetailPage.jsx
+++ src/Pages/HotelDetailPage/HotelDetailPage.jsx
@@ -6,7 +6,12 @@ export default function HotelDetailPage(props) {
 
     return (
         <div>
-            <img style={{ width: '300px' }} src={props.hotelPhotos.hotelImages[0].baseUrl.replace('_{size}', '')} alt="Hotel Image"/>
+            {props.hotelPhotos.hotelImages.map((h, idx) => 
+                {if (idx < 4) {
+                    return <img style={{width: '300px'}} src={h.baseUrl.replace('_{size}', '')} alt="Hotel Image"/>
+                }}
+                )}
+            {/* <img style={{ width: '300px' }} src={props.hotelPhotos.hotelImages[0].baseUrl.replace('_{size}', '')} alt="Hotel Image"/> */}
             <p>Booking URL: {props.oneHotel.roomsAndRates.bookingUrl}</p>
             <p>Amenities:</p>
             <ul>{props.oneHotel.overview.overviewSections[0].content.map(c => <li>{c}</li>)}</ul>
@@ -17,11 +22,10 @@ export default function HotelDetailPage(props) {
             <p>Name: {props.oneHotel.propertyDescription.name}</p>
             <p>Star: {props.oneHotel.propertyDescription.starRating}</p>
             <p>Price: {props.oneHotel.propertyDescription.featuredPrice.currentPrice.plain}</p>
-            <p>Star:</p>
+            <p>Room Types:</p>
             <ul>{props.oneHotel.propertyDescription.roomTypeNames.map(r => <li>{r}</li>)}</ul>
             <p>Rating: 
                 {props.oneHotel.guestReviews.brands.rating} <br />
-                {props.oneHotel.guestReviews.brands.badgeText} <br />
                 Number of ratings: {props.oneHotel.guestReviews.brands.total}
             </p>
 
