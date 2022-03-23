fae1b12714114802e75b62ef3fbb8bb748c8d563
JChapman13 <77743972+JChapman13@users.noreply.github.com>
2022-03-23 00:00:55 -0400

    conflict resolution in components

--- config/database.js
+++ config/database.js
@@ -1,21 +1,12 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-<<<<<<< HEAD
-// mongoose.connect(process.env.DATABASE_URL, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-// })
-
-mongoose.connect('mongodb://localhost/tripbudget', {
-=======
 mongoose.connect(process.env.DATABASE_URL, {
->>>>>>> 42f06f696d4262adfc1eeb74d7ea231b67f3558a
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 
-db.on('connected', function () {
-    console.log(`Connected to ${db.name} at ${db.host}:${db.port}`);
+db.on("connected", function() {
+  console.log(`Connected to ${db.name} at ${db.host}:${db.port}`);
 });
--- src/index.js
+++ src/index.js
@@ -1,28 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import './index.css';
-=======
->>>>>>> 42f06f696d4262adfc1eeb74d7ea231b67f3558a
-import App from './Pages/App/App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./Pages/App/App";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
-<<<<<<< HEAD
   <React.StrictMode>
     <Router>
       <App />
     </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-=======
-	<React.StrictMode>
-		<Router>
-			<App />
-		</Router>
-	</React.StrictMode>,
-	document.getElementById('root')
-);
->>>>>>> 42f06f696d4262adfc1eeb74d7ea231b67f3558a
