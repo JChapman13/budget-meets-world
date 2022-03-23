39dd0c002c289d2a7a754c56b3a2edb09055f4db
JChapman13 <77743972+JChapman13@users.noreply.github.com>
2022-03-23 14:53:42 -0400

    adding category toggle values

--- src/Components/Flights/Flights.jsx
+++ src/Components/Flights/Flights.jsx
@@ -4,6 +4,17 @@ import Flight from "../Flight/Flight";
 const axios = require("axios").default;
 
 export default function Flights(props) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (props.flights.length !== 0) {
+      setIsLoading(false);
+    }
+  }, [props.flights]);
+
+  if (isLoading) {
+    return <h1>Loading</h1>;
+  }
   return (
     <div className="Flights">
       {props.flights.map((f, idx) => {
