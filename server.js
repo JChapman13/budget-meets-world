2bd6d56fc05797fe1ada2637ef247e6c68fb5033
JChapman13 <77743972+JChapman13@users.noreply.github.com>
2022-03-23 00:00:55 -0400

    conflict resolution in components

--- server.js
+++ server.js
@@ -1,62 +1,30 @@
-<<<<<<< HEAD
-const express = require('express')
-const path = require('path')
-const favicon = require('serve-favicon')
-const logger = require('morgan')
-const app = express()
-const http = require('http')
-const server = http.createServer(app)
-require('dotenv').config()
-require('./config/database')
-
-app.use(logger('dev'))
-app.use(express.json())
-
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')))
-app.use(express.static(path.join(__dirname, 'build')))
-
-app.use('/api/flights', require('./routes/api/flights.js'))
-app.use('/api/hotels', require('./routes/api/hotels.js'))
-app.use('/api/foods', require('./routes/api/foods.js'))
-app.use('/api/users', require('./routes/api/users.js'))
-
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
-=======
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 // const favicon = require('serve-favicon')
-const logger = require('morgan');
+const logger = require("morgan");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
-require('dotenv').config();
-require('./config/database');
+require("dotenv").config();
+require("./config/database");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 
 // app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')))
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "build")));
 
-app.use('/api/flights', require('./routes/api/flights.js'));
-app.use('/api/hotels', require('./routes/api/hotels.js'));
-app.use('/api/foods', require('./routes/api/foods.js'));
-app.use('/api/users', require('./routes/api/users.js'));
+app.use("/api/flights", require("./routes/api/flights.js"));
+app.use("/api/hotels", require("./routes/api/hotels.js"));
+app.use("/api/foods", require("./routes/api/foods.js"));
+app.use("/api/users", require("./routes/api/users.js"));
 
-app.get('/*', function(req, res) {
-	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
->>>>>>> 42f06f696d4262adfc1eeb74d7ea231b67f3558a
 
 const port = process.env.PORT || 3001;
 
 server.listen(port, function() {
-<<<<<<< HEAD
-    console.log(`Express app running on port ${port}`)
+  console.log(`Express app running on port ${port}`);
 });
-=======
-	console.log(`Express app running on port ${port}`);
-});
->>>>>>> 42f06f696d4262adfc1eeb74d7ea231b67f3558a
--- src/Components/Hotels/Hotels.jsx
+++ src/Components/Hotels/Hotels.jsx
@@ -1,38 +1,13 @@
-import './Hotels.css'
-import React from 'react'
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 42f06f696d4262adfc1eeb74d7ea231b67f3558a
-
-
+import "./Hotels.css";
+import React from "react";
+import Hotel from "../Hotel/Hotel";
 
 export default function Hotels(props) {
-
-
-    return (
-        <div className='Hotels'>
-            <p>Hotels</p>
-<<<<<<< HEAD
-=======
-=======
-import Hotel from '../Hotel/Hotel'
-
-
-export default function Hotels(props) {
-    
-
-    return (
-        <div className='Hotels'>
-            {props.hotels.map(hotel => 
-                
-                    <Hotel 
-                        hotel={hotel}
-                    />
-                
-            )}
->>>>>>> 77ada14e0d7a91ed162f4989a9902f406908e9ce
->>>>>>> 42f06f696d4262adfc1eeb74d7ea231b67f3558a
-        </div>
-    )
+  return (
+    <div className="Hotels">
+      {props.hotels.map((hotel) => (
+        <Hotel hotel={hotel} />
+      ))}
+    </div>
+  );
 }
