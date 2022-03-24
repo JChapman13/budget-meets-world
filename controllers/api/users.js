2991aa0b23ffa5cb3b9b2c0706534fd169e65be3
Joanne Kam <joannekam@Joannes-MacBook-Pro.local>
2022-03-24 10:47:21 -0400

    saving

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -155,14 +155,14 @@ async function saveHotel(req, res) {
 	}
 }
 
-async function saveFlight(req, res) {
-	console.log(req, 'saveFlight req');
+async function saveFlight(object) {
+	console.log(req.body, 'saveFlight req');
 	try {
 		const user = await UserModel.findById(req.body.userId);
 		const trip = await user.trip.find((trip) => trip._id == req.body.tripId);
-        await trip.flight.push({req.body})
-        await trip.save()
-        res.status(200).json({ user: user, trip: trip})
+		await trip.flight.push({ object });
+		await trip.save();
+		res.status(200).json({ user: user, trip: trip });
 	} catch (err) {
 		res.status(400).json(err);
 	}
